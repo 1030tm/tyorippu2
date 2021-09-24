7230795516793c1f754e737542a52f21f6fd9c84
1030tm <moninoki0139@gmail.com>
2021-09-24 13:28:17 +0900

    コントローラーの設定

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,27 @@
 class PagesController < ApplicationController
+
   def index
+    @pages = Page.all_blank
+  end
+
+  def new
+    @page = Page.new
+    @page.spots.build
+  end
+
+  def create
+    @page = Page.new(page_params)
+    if @page.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:page_title, :page_comment,
+      spots_attributes:[:id, :spot_name, :spot_address, :spot_tel, :spot_parking, :spot_comment, :_destroy, :page_id]).merge(user_id: current_user.id)
   end
 end
