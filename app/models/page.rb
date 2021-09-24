b07386c9e3d00d5d912d2986edbb49f964db923b
1030tm <moninoki0139@gmail.com>
2021-09-24 12:39:22 +0900

    アソシエーションの設定

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,7 @@
 class Page < ApplicationRecord
+  belongs_to :user
+  has_many :spots, dependent: :destroy, inverse_of: :page
+  accepts_nested_attributes_for :spots, allow_destroy: true, reject_if: :all_blank
+
+  validates :page_title, presence: true
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,7 @@
 class Spot < ApplicationRecord
+  belongs_to :page
+
+  validates :spot_name, presence: true
+  validates :spot_address, presence: true
+  validates :spot_parking, presence: true
 end
