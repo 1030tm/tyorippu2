7f6cf2a293b1a73a0091d6275797300d463ecd71
1030tm <moninoki0139@gmail.com>
2021-09-30 00:53:15 +0900

    showのcss追加

--- app/javascript/timeline.js
+++ app/javascript/timeline.js
@@ -8,13 +8,15 @@ function ScrollTimelineAnime(){
     let windowHeight = $(window).height();// windowの高さ取得
     let startPoint = 0; //線をスタートさせる位置を指定※レイアウトによって調整してください
 
+    console.log(elemPos);
+
     if (scroll >= elemPos - windowHeight-startPoint){       
       let H = $(this).outerHeight(true)//liの余白と高さを含めた数値を取得
       //スクロール値から要素までの高さを引いた値を、liの高さの半分のパーセントで出す
       let percent = (scroll+startPoint - elemPos) / (H/2) * 100;//liの余白と高さの半分で線を100％に伸ばす
 
       // 100% を超えたらずっと100%を入れ続ける
-      if(percent  > 100 || 0 > percent){
+      if(percent < 0 || percent > 100){
         percent  = 100;
       }
 
@@ -22,6 +24,7 @@ function ScrollTimelineAnime(){
       $(this).children('.border-line').css({
         height: percent + "%", //CSSでパーセント指定
       });
+
     } 
   });
 }
@@ -29,9 +32,5 @@ function ScrollTimelineAnime(){
 // 画面をスクロールをしたら動かしたい場合の記述
 $(window).on('scroll', function(){
   ScrollTimelineAnime();// 線が伸びる関数を呼ぶ
-});
 
-// ページが読み込まれたらすぐに動かしたい場合の記述
-//$(window).on('load', function(){
-//  ScrollTimelineAnime();// 線が伸びる関数を呼ぶ
-//});
+});
