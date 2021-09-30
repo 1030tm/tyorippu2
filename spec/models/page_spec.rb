98767bd033feb1ff51991b3d2471da5e61e69d03
1030tm <moninoki0139@gmail.com>
2021-09-30 13:21:51 +0900

    pageのモデル単体テスト

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Page, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @page = FactoryBot.build(:page)
+  end
+
+  describe 'ページ新規投稿' do
+    context '新規投稿ができるとき' do
+      it 'page_title,page_comment,user_idがあれば投稿できる' do
+        expect(@page).to be_valid
+      end
+    end
+
+    context '新規投稿ができないとき' do
+      it 'page_titleが空だと投稿できない' do
+        @page.page_title = ''
+        @page.valid?
+        expect(@page.errors.full_messages).to include("Page title can't be blank")
+      end
+      it 'userと紐づいていないと登録できない' do
+        @page.user = nil
+        @page.valid?
+        expect(@page.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
