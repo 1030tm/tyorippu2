36bfb01e9dbea220e68fef9756d81e697d7db19a
1030tm <moninoki0139@gmail.com>
2021-09-30 13:53:51 +0900

    pageコントローラーの単体テスト

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_page, only: [:destroy, :edit, :update]
 
   def index
